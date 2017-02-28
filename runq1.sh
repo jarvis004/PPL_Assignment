c1e76bcf702b2cc4010358a5eb9383fc849c0ab0
Mritunjay Chaudhary <mkdd10@gmail.com>
2017-02-28 12:29:02 +0530

    Add files via upload

---
+++ runq1.sh
@@ -0,0 +1,10 @@
+# checking wheather log.txt file exist or not (if already exist then removes first )
+file="log.txt"
+if [ -f $file ] ; then
+		rm $file
+fi
+# compiling java files
+javac *.java
+# passed the main class to command line argument
+java q1
+
